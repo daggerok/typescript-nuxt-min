b576a595b03b324e4c4c599c1b2f4e5c6062c691
Maksim Kostromin <daggerok@gmail.com>
2020-05-01 17:41:14 +0300

    I would rather be playing SC2. (C) whatthecommit.com

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,5 +1,84 @@
+const baseHref = process.env.BASE_HREF || '/';
+const title = process.env.npm_package_name || ''
+const content = process.env.npm_package_description || '';
+
 export default {
+  // mode: 'universal',
+
+  head: {
+    title,
+    meta: [
+      { charset: 'utf-8' },
+      { name: 'viewport', content: 'width=device-width, initial-scale=1' },
+      { hid: 'description', name: 'description', content },
+    ],
+    link: [
+      { rel: 'icon', type: 'image/x-icon', href: baseHref + 'favicon.ico' },
+    ]
+  },
+
+  // /**
+  //  *  Customize the progress-bar color
+  //  */
+  // loading: { color: '#fff' },
+
+  // /**
+  //  *  Global CSS
+  //  */
+  // css: [],
+
+  // plugins: [],
+
+  /**
+   * Nuxt dev-modules
+   */
   buildModules: [
     '@nuxt/typescript-build',
+    // '@nuxtjs/router',
   ],
+
+  extends: [
+    '@nuxtjs/eslint-config-typescript',
+  ],
+
+  typescript: {
+    typeCheck: {
+      eslint: true,
+    },
+  },
+
+  // /**
+  //  * Nuxt modules
+  //  */
+  // modules: [
+  //   // // '@nuxtjs/pwa',
+  //   // '@nuxtjs/axios',
+  //   // '@nuxtjs/dotenv',
+  // ],
+
+  // /**
+  //  * Axios
+  //  */
+  // axios: { },
+
+  // /**
+  //  * Extend build
+  //  */
+  // build: {
+  //   /*
+  //   ** You can extend webpack config here
+  //   */
+  //   extend (config, ctx) { }
+  // },
+
+  // generate: {
+  //   routes: [
+  //     '/',
+  //   ],
+  // },
+
+  // router: {
+  //   base: baseHref,
+  //   mode: 'history',
+  // },
 };
--- pages/index.vue
+++ pages/index.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="app">
-    <h1>Hello, {{ id }}!</h1>
+    <h1>Hello, {{ id }} and option API!</h1>
   </div>
 </template>
 
